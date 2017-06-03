429008a739f11ffbc1be464586e5a5fde3c71f94
Steven Solomon <stevensolomon@Stevens-MacBook-Pro.local>
2017-06-03 10:53:46 -0400

    Convert Drawer into class

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ const Measures = require('./measures');
 var measures = new Measures(1, 5);
 measures.add('example', 2);
 
-function Drawer() {
+class Drawer {
+  draw(measure) { console.log(measure) }
 }
-
-Drawer.prototype.draw = function(measure) { console.log(measure) }
 
 measures.draw(new Drawer());
