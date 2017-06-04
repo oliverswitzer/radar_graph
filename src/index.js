6be225c91656c17f40bb245528b51fd592e65d76
Steven Solomon <stevensolomon@Stevens-MacBook-Pro.local>
2017-06-04 12:58:52 -0400

    Extract LabelsDrawer

--- src/index.js
+++ src/index.js
@@ -9,50 +9,54 @@ measures.add('simplicity', 1);
 
 const c = document.getElementById('svg-container');
 class TransformDrawer {
-  constructor(container, drawer) {
+  constructor(container, drawers) {
     this._svgNameSpace = 'http://www.w3.org/2000/svg';
     this._container = container;
-    this._drawer = drawer;
+    this._drawers = drawers;
   }
 
   draw(measure) {
-    const container = this._container;
     const wrapper = document.createElementNS(this._svgNameSpace, 'g');
-    container.appendChild(wrapper);
+    this._container.appendChild(wrapper);
 
-    this._drawer.draw(wrapper, measure);
+    this._drawers.forEach((d) => d.draw(wrapper, measure));
 
-    this._makeLabel(wrapper, measure);
     wrapper.setAttribute('transform', `rotate(${measure.angle}, ${box.center.x}, ${box.center.y})`);
-   }
-
-   _makeLabel(container, measure) {
-     const text = document.createElementNS(this._svgNameSpace, 'text');
-     const circleElement = document.querySelector(`[data-name='${measure.name}']`);
-     const rect = circleElement.getBoundingClientRect();
-
-     text.setAttribute('x', measure.x);
-     text.setAttribute('y', measure.y);
-     text.setAttribute('fill', '#ff0000');
-     text.setAttribute('transform', `rotate(${measure.angle * -1}, ${rect.left}, ${rect.top})`);
-     text.textContent = measure.name;
-     container.appendChild(text);
    }
 }
 
 class PointsDrawer {
-  draw(container, measure) {
-    this._makeCircle(container, measure);
+  constructor() {
+    this._svgNameSpace = 'http://www.w3.org/2000/svg';
   }
 
-  _makeCircle(container, measure) {
-    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  draw(container, measure) {
+    const circle = document.createElementNS(this._svgNameSpace, 'circle');
     circle.setAttribute('cx', measure.x);
     circle.setAttribute('cy', measure.y);
     circle.setAttribute('data-name', measure.name);
     circle.setAttribute('r', 2);
     circle.setAttribute('stroke', '#ff0000');
     container.appendChild(circle);
+  }
+}
+
+class LabelsDrawer {
+  constructor() {
+    this._svgNameSpace = 'http://www.w3.org/2000/svg';
+  }
+
+  draw(container, measure) {
+    const text = document.createElementNS(this._svgNameSpace, 'text');
+    const circleElement = document.querySelector(`[data-name='${measure.name}']`);
+    const rect = circleElement.getBoundingClientRect();
+
+    text.setAttribute('x', measure.x);
+    text.setAttribute('y', measure.y);
+    text.setAttribute('fill', '#ff0000');
+    text.setAttribute('transform', `rotate(${measure.angle * -1}, ${rect.left}, ${rect.top})`);
+    text.textContent = measure.name;
+    container.appendChild(text);
   }
 }
 
@@ -108,4 +112,4 @@ class LinesDrawer {
   //    return array;
   //  }
 measures.draw('lines', new LinesDrawer(c));
-measures.draw('points', new TransformDrawer(c, new PointsDrawer()));
+measures.draw('points', new TransformDrawer(c, [new PointsDrawer(), new LabelsDrawer()]));
